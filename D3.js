b9910f892b212c4e67363030eb6c0f2129183439
sonomastea <sbstea@usf.edu>
2024-06-14 15:07:42 -0400

    Implement interactive behaviors with D3.js

--- D3.js
+++ D3.js
@@ -1,1 +1,16 @@
+U05721483
+document. AddEventListener ("DOMContent- Loaded", function (){
+    const data = [1, 2, 3, 4, 5]; //Example Data 
 
+    const container= d3.select("body")
+                    .append ("div")
+                    .attr("id", "container");
+
+
+                    container.selectAll('.data-point"')
+                           .data(data)
+                           .enter()
+                           .append("div")
+                           .attr("class", "data-point")
+                           .text(d=> d);
+})
